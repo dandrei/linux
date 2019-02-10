382bcf36564842e3f8d0ebed8d96c0dff55e6b94
Andrei <350897+dandrei@users.noreply.github.com>
2019-02-10 14:32:26 +0200

    Added systeminfo.sh documentation

--- systeminfo.sh
+++ systeminfo.sh
@@ -1,3 +1,7 @@
+# This script outputs basic hardware info about your system: CPU, Ram, Disk type (SSD or HDD)
+# To run directly from the command line:
+# curl https://raw.githubusercontent.com/dandrei/linux/master/systeminfo.sh 2> /dev/null | bash
+
 _cpu=$(lscpu | grep "Model name" | sed 's/Model name: //g')
 _ram=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
 _rot=$(cat /sys/block/sda/queue/rotational)
